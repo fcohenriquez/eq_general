31955e7a0dcde5f37272d7a22523d5b00f84585a
fcohenriquez <henriquez.fco@gmail.com>
2025-07-24 11:32:59 -0400

    Se estimaron los parametros gamma y beta  aprtir de los datos y se mejoraron los resultados de partirda de la base de impuesto a la renta (PIT)

--- cge_calib_chile_2008_simpl.model.R
+++ cge_calib_chile_2008_simpl.model.R
@@ -1,10 +1,10 @@
-# Generated on 2025-07-23 16:54:31 by gEcon ver. 1.2.3 (2025-04-13)
+# Generated on 2025-07-24 11:04:22 by gEcon ver. 1.2.3 (2025-04-13)
 # http://gecon.r-forge.r-project.org/
 
 # Model name: cge_calib_chile_2008_simpl
 
 # info
-info__ <- c("cge_calib_chile_2008_simpl", "D:/francisco.henriquez/Mis documentos/proyectos/equilibrio_general/eq_general/cge_calib_chile_2008_simpl.gcn", "2025-07-23 16:54:31", "false")
+info__ <- c("cge_calib_chile_2008_simpl", "D:/francisco.henriquez/Mis documentos/proyectos/equilibrio_general/eq_general/cge_calib_chile_2008_simpl.gcn", "2025-07-24 11:04:22", "false")
 
 # index sets
 index_sets__ <- list(PRODS = c("Prod"),
